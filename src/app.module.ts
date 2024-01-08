ff3c17f3dd328b89f28b0cf326ff339104cab830
Wilfredo Sulbaran <wilfredosulbaran94@gmail.com>
2024-01-08 15:06:10 -0500

    feature: add unit test and e2e test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RecordsModule } from '@records/records.module';
+import { RecordsModule } from './modules/records/records.module';
 
 @Module({
   imports: [RecordsModule],
--- src/modules/records/records.service.spec.ts
+++ src/modules/records/records.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecordsService } from './records.service';
-
+import * as data from '../../utils/mock/data.json';
 describe('RecordsService', () => {
   let service: RecordsService;
 
@@ -14,5 +14,43 @@ describe('RecordsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('Error if array Records is empty', () => {
+    expect(() =>
+      service.create({
+        Records: [],
+      }),
+    ).toThrow('Records can not be empty!!!');
+  });
+  it('should return array records objects', () => {
+    const result = service.create(data);
+    expect(result).toHaveLength(1);
+  });
+
+  it('should create records with valid input', () => {
+    const result = service.create(data);
+    expect(result).toHaveLength(1);
+    expect(result[0]).toHaveProperty('spam', true);
+    expect(result[0]).toHaveProperty('virus', true);
+    expect(result[0]).toHaveProperty('dns', true);
+    expect(result[0]).toHaveProperty('retrasado', false);
+    expect(result[0]).toHaveProperty('mes', 'September');
+    expect(result[0]).toHaveProperty(
+      'emisor',
+      '61967230-7A45-4A9D-BEC9-87CBCF2211C9',
+    );
+    expect(result[0]).toHaveProperty('receptor');
+    expect(result[0].receptor).toHaveLength(1);
+  });
+
+  it('Validate if emisor and receptor not have domain', () => {
+    const result = service.create(data);
+    const whitoutDomain1 = '61967230-7A45-4A9D-BEC9-87CBCF2211C9';
+    const whitoutDomain2 = 'recipient';
+
+    expect(result[0]).toHaveProperty('emisor');
+    expect(result[0].emisor).toBe(whitoutDomain1);
+    expect(result[0].receptor[0]).toBe(whitoutDomain2);
   });
 });
